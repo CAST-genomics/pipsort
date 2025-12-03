93055aca422829d6904ee80a257842042fd4d3e9
Tara Mirmira <tmirmira@gmail.com>
2025-12-02 16:54:11 -0800

    add unit test

---
+++ tests/small_example/run_test.sh
@@ -0,0 +1,3 @@
+#../../PIPSORT -l ldfiles.txt -z zfiles.txt -m eur_afr_small_test_snp_map -n 7000,7000 -q 1 -o mscaviar_results
+../../PIPSORT -l ldfiles.txt -z zfiles.txt -m eur_afr_small_test_snp_map -n 7000,7000 -o mscaviar_results
+
